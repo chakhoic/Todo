cc58eb5a770f13414cd5c0fa274973c882f9b93e
chak <chakhoichan@gmail.com>
2023-11-14 17:49:40 -0500

    css

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
-
+export class TodosComponent implements OnInit{
+  title = 'title!';
+  todos!: Todo[];
+  constructor() {}
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: "1st todo",
+        completed: false
+      }, {
+        content: "2nd todo",
+        completed: true
+      }
+    ]
+  }
 }
