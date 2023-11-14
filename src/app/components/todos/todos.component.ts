7805980fa070fd975d841344b322de5a9083cf2c
chak <chakhoichan@gmail.com>
2023-11-14 18:20:07 -0500

    stage1done

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -7,8 +7,9 @@ import { Todo } from 'src/app/models/Todo';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit{
-  title = 'title!';
+  title = 'TODOS!';
   todos!: Todo[];
+  inputTodo:string =''
   constructor() {}
   ngOnInit(): void {
     this.todos = [
@@ -27,4 +28,17 @@ export class TodosComponent implements OnInit{
       return v;
     })
   }
+
+  deleteTodo (id: number) {
+    this.todos = this.todos.filter((v,i) => i !== id);
+  }
+
+  addTodo () {
+    this.todos.push ({
+      content: this.inputTodo,
+      completed: false
+    });
+    this.inputTodo = "" // word doesnt stay at form
+  }
+
 }
